616082c50038b0e9a2186059dae6f2c57ee86080
Michielu Menning <mich.menning@gmail.com>
2020-07-18 10:37:23 -0500

    Prepare for deploy

--- server/src/index.js
+++ server/src/index.js
@@ -7,13 +7,17 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const setRoutes = require("./routes");
+const path = require('path');
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 setRoutes(app, io)
-app.use('/', express.static(__dirname + '/'));
+app.get('/*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+})
 
 io.on("connection", (socket) => {
     console.log("New client connected");
